75551281f393a49650bca62a0b9466664e8aedac
Spikey21 <rafal.paciorek19@gmail.com>
2025-02-21 16:16:58 +0100

    model update

---
+++ model/quoteModel.js
@@ -0,0 +1,45 @@
+const { ObjectId } = require("mongodb")
+const mongoSinleton = require("../data/mongoDBSignleton")
+const ObjectId = require("mongodb").ObjectId
+
+function saveAll(quotes) {
+    return new Promise (async (resolve, reject) => {
+        const collection = await mongoSinleton.getCollection()
+        const result = await collection.insertMany(quotes)
+        if (result.insertedCount) {
+            resolve(result)
+        } else {
+            reject("couldnt save quotes")
+        }
+    })
+}
+
+function getAll() {
+    return new Promise (async (resolve, reject) => {
+        const collection = await mongoSinleton.getCollection()
+        const cursor = collection.find()
+        const result = await cursor.toArray()
+        if (result.length>0) {
+            resolve(result)
+        } else {
+            reject("couldnt get quotes")
+        }
+    })
+}
+
+function getById(id) {
+    return new Promise (async (resolve, reject) => {
+        const collection = await mongoSinleton.getCollection()
+        const result = await collection.findOne({_id: ObjectId(id)})
+        if (result) {
+            resolve(result)
+        } else {
+            reject("couldnt get quote by Id" + id)
+        }
+    })
+}
+module.exports = {
+    getAll,
+    getById,
+    saveAll
+}

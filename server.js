42de3da4f8cd4613a19911b50e304aa871851c99
Spikey21 <rafal.paciorek19@gmail.com>
2025-03-12 16:35:34 +0100

    add fncs to server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http")
 
-const {getQuotes, getQuote, getRandom, prepareDB} = require("./controllers/quoteController")
+const {getQuotes, getQuote, getRandom, prepareDB, insertQuote, deleteQuote} = require("./controllers/quoteController")
 const {serveStaticFile} = require("./util/staticServer")
 
 const PORT = 8080
@@ -43,6 +43,48 @@ const server = http.createServer(
                 quote = {message: "Quote not found"}
             }
             res.end(JSON.stringify(quote))
+        } else 
+        if (req.url === "/api/quote/save" && req.method === "POST") {
+            let data = ""
+            req.on("data", function (chunk) {
+                data+= chunk
+            })
+            req.on("end", async function () {
+                const quote = JSON.parse(data)
+                let response = {}
+                const result = insertQuote(quote)
+                if (result) {
+                    res.writeHead(200, API_CONTENT_TYPE)
+                    response = {saved: true, _id: result.insertedId}
+                } else {
+                    res.writeHead(404, API_CONTENT_TYPE)
+                    response = {saved: false, _id: null}
+                }
+                res.end(JSON.stringify(response))
+            })
+        } else 
+        if (req.url === "/api/quote/delete" && req.method === "POST") {
+            let data = ""
+            req.on("data", function (chunk) {
+                data+= chunk
+            })
+            req.on("end", async function () {
+                const quote = JSON.parse(data)
+                if (!quote || !quote._id) {
+                    res.writeHead(404, API_CONTENT_TYPE)
+                    response = {message: "Quote not found"}
+                } else {
+                    let response = {}
+                    const result = await deleteQuote(quote._id)
+                    if (result && result.deletedCount > 0) {
+                        res.writeHead(200, API_CONTENT_TYPE)
+                        response = {deleted: true,}
+                    }
+                    res.writeHead(404, API_CONTENT_TYPE)
+                    response = {deleted: false}
+                }
+                res.end(JSON.stringify(response))
+            })
         } else {
             serveStaticFile(req, res)
         }

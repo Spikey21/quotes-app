9160f3e6efd5fc0d6edb119ef6339b6aed38fd31
Spikey21 <rafal.paciorek19@gmail.com>
2025-03-24 17:10:47 +0100

    save func to editor

--- static/editor.js
+++ static/editor.js
@@ -11,6 +11,11 @@ class QuoteEditor {
         document.addEventListener("keyup", e => {
             if (e.key === "e") this.showEditor() 
         })
+        
+        document.querySelector("form").addEventListener("submit", e => {
+            e.preventDefault()
+            this.processNewQuote()
+        })
     }
 
     showEditor = async () => {
@@ -64,6 +69,31 @@ class QuoteEditor {
     removeAllChildNodes(parent) {
         while(parent.firstChild) {
             parent.removeChild(parent.firstChild)
+        }
+    }
+
+    processNewQuote = async () => {
+        if (this.quoteText.value.length === 0 || this.quoteAuthor.value.length === 0) {
+            console.error("Data not sufficient")
+            return
+        }
+        const response = await fetch("/api/quote/save", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                quote: this.quoteText.value,
+                author: this.quoteAuthor.value
+            })
+        })
+
+        const data = await response.json()
+        if (data && data.saved === true) {
+            this.reloadQuotesList()
+            this.quoteText.value = ""
+            this.quoteAuthor.value = ""
         }
     }
 }
